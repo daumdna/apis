5ec71e6dd7580a9a7e97f48aae50f5e3da0a0487
이승철 <tadoli2@daumcorp.com>
2012-05-24 14:42:54 +0900

    iOS용 WebView 예제 디렉토리명 변경

---
+++ Samples/6.Maps/tip/WebViewSample-iOS/Classes/WebViewSampleViewController.h
@@ -0,0 +1,18 @@
+//
+//  WebViewSampleViewController.h
+//  WebViewSample
+//
+//  Created by 승철 이 on 12. 5. 22..
+//  Copyright 다음 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WebViewSampleViewController : UIViewController {
+	IBOutlet UIWebView *webView;
+}
+
+@property (nonatomic,retain) IBOutlet UIWebView *webView;
+
+@end
+
---
+++ Samples/6.Maps/tip/WebViewSample-iOS/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WebViewSample
+//
+//  Created by 승철 이 on 12. 5. 22..
+//  Copyright 다음 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
